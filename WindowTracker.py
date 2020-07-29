1f9413b08419220c62cc3679cd7d1a9769a65b1e
Bitsu Mamo <bitsu123@gmail.comm>
2020-07-29 17:46:45 +0300

    Added TODOs and simplified logic for browser check

--- WindowTracker.py
+++ WindowTracker.py
@@ -1,37 +1,34 @@
-#TODO Add json parsing and logic
+#TODO: Add json parsing and logic
+#TODO: Add a filter to eliminate unessecary windows eg: New Tab, TaskManager...
+#TODO: Add filter to check if same app with different windows
+# dataHolder just temp till I know Json
+from FilterList import FilterList as Fl
 from win32gui import GetWindowText, GetForegroundWindow
 import time
 from datetime import datetime
+
 class WindowTracker(object):
-    def __init__(self, window, delayTime, browsers, unWanted, timeUsed):
-        self.window = window
+
+    def __init__(self, browserList, dataHolder, delayTime):
+        self.browserList = browserList
+        self.dataHolder = dataHolder
         self.delayTime = delayTime
-        self.browsers = browsers
-        self.unWanted = unWanted
-        self.timeUsed = timeUsed
 
-    def isBrowser(self):
-        for browser in self.browsers:
-            if self.window.endswith(browser):
-                self.window = self.window.split('-')
-                self.window = self.window[-2].strip()
-    #TODO this shit ain't working
-    def isWanted(self):
-        for item in self.unWanted:
-            if str(self.window).strip() != str(item).strip():
-                self.timeUsed[self.window] = datetime.now().second
-
+    def checkIfBrowser(self, window):
+        for browser in self.browserList:
+            if window.endswith(browser):
+                window = window.split('-')
+                return window[-2]
+        return window
     def run(self):
         while True:
-            self.window = GetWindowText(GetForegroundWindow())
-            self.isBrowser()
-            self.isWanted()
-            print(self.timeUsed)
+            activeWindow = GetWindowText(GetForegroundWindow())
+            activeWindow = self.checkIfBrowser(activeWindow)
+            self.dataHolder[activeWindow] = datetime.now().second
+            print(self.dataHolder)
             time.sleep(self.delayTime)
 
-unWanted = ['New Tab', 'Google Search', 'Task Manager', ' ', '']
-browsers = ['Brave', 'Chrome', 'FireFox']
-timeUsed = {}
 
-w = WindowTracker(GetWindowText(GetForegroundWindow()), 2, browsers, unWanted, timeUsed)
+dataHolder = {}
+w = WindowTracker(Fl.browsers, dataHolder, 1)
 w.run()
