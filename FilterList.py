d3cd18661d380ac9e041bb241b8b6453a6c12d28
Bitsu Mamo <bitsu123@gmail.comm>
2020-07-30 15:27:05 +0300

    Added a FilterList class to contain all list to filter through.

---
+++ FilterList.py
@@ -0,0 +1,15 @@
+class FilterList(object):
+
+    unWanted = ['New Tab', 'Google Search', 'Task Manager', ' ', '']
+
+    browsers = ['Brave', 'Chrome', 'FireFox']
+
+    sameApp = [
+        {
+            'Zoom Cloud Meetings': 'Zoom',
+            'Waiting for Host': 'Zoom'
+        },
+        {
+            'Telegram (***)': 'Telegram'
+        }
+    ]
