1144c8bf3ac1b65bd9194b95b26896d12d3dd592
SirPotato10000 <mulat.simon@gmail.com>
2020-11-29 17:11:49 +0300

    Added extra fields to the db table

--- time_tracker/serializer.py
+++ time_tracker/serializer.py
@@ -1,8 +1,9 @@
 import os
 import sqlite3
 
-from datetime import datetime
+from datetime import date, datetime
 from sqlite3.dbapi2 import Connection, Cursor
+from time import time
 from typing import Dict, List, Optional, Tuple
 
 # Constansts
@@ -26,9 +27,11 @@ def get_db_cursor() -> Cursor:
         m_db_con = sqlite3.connect(DB_PATH)
     return m_db_con.cursor()
 
+
 def commit():
     global m_db_con
     m_db_con.commit()
+
 
 def close_db_connection() -> None:
     global m_db_con
@@ -38,29 +41,55 @@ def close_db_connection() -> None:
 
 # Creates the main table in the db
 def init_db(db_con: Connection) -> None:
-    db_con.cursor().execute(
-        "CREATE TABLE IF NOT EXISTS activities (application text, start_date text, end_date text)"
+    table_init_str = "".join(
+        [
+            "CREATE TABLE IF NOT EXISTS activities (application text, start_date_iso",
+            " text, end_date_iso text, start_year integer, start_month integer, start_day integer,",
+            " start_time text, end_year integer, end_month integer, end_day integer, end_time text)",
+        ]
     )
+    db_con.cursor().execute(table_init_str)
 
 
 # Adds the current activity values into the db
 def save_to_db(activity_data: List[Tuple[str, datetime, datetime]]) -> None:
     conn = get_db_cursor()
+    formatted_data = [generate_values(entry) for entry in activity_data]
     conn.executemany(
-        "INSERT INTO activities VALUES (?, ?, ?)",
-        [
-            (entry[0], entry[1].isoformat(), entry[2].isoformat())
-            for entry in activity_data
-        ],
+        "INSERT INTO activities VALUES (?, ?, ?, ?, ?, ?, ?, ?, ? ,?, ?)",
+        formatted_data,
     )
     commit()
-    
+
+
+DB_VALUE_TYPE = Tuple[str, str, int, int, int, str, str, int, int, int, str]
+
+
+def generate_values(entry: Tuple[str, datetime, datetime]) -> DB_VALUE_TYPE:
+    app_name = entry[0]
+    start_date = entry[1]
+    end_date = entry[2]
+    return (
+        app_name,
+        start_date.isoformat(),
+        end_date.isoformat(),
+        start_date.year,
+        start_date.month,
+        start_date.day,
+        f"{start_date.hour}:{start_date.minute}:{start_date.second}",
+        end_date.year,
+        end_date.month,
+        end_date.day,
+        f"{end_date.hour}:{end_date.minute}:{end_date.second}",
+    )
 
 
 # Returns all the acitvites logged in the database
 def get_all_data() -> List[Tuple[str, datetime, datetime]]:
     conn = get_db_cursor()
-    data = conn.execute("SELECT * FROM activities ORDER BY start_date ASC").fetchall()
+    data = conn.execute(
+        "SELECT * FROM activities ORDER BY start_date_iso ASC"
+    ).fetchall()
     return [
         (entry[0], datetime.fromisoformat(entry[1]), datetime.fromisoformat(entry[2]))
         for entry in data
